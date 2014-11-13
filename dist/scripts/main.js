f297e5721d3043f7742be84fd6f556c588f89c30
Jess Scheuring <jscheuring@gmail.com>
2014-11-13 13:56:45 -0500

    build

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -45,15 +45,15 @@ Pecan.ApplicationController = Ember.Controller.extend({
     });
     this.set('order', order);
   },
-  // productSelected: function() {
-  //   var product = (this.store.find('product',
-  //     this.get('order').get('orderItems'))
-  //     .then(function(data) {
-  //       return data;
-  //     }));
-  //   console.log(product);
-  //   return product;
-  //   }.property('order'),
+  productSelected: function() {
+    var product = (this.store.find('product',
+      this.get('order').get('orderItems'))
+      .then(function(data) {
+        return data;
+      }));
+    console.log(product);
+    return product;
+    }.property('order'),
   itemsCount: function() {
     return this.get('controllers.checkout.product').length;
   }.property('orderItems'),
